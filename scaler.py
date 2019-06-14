270ab528436dff0748910fcfcfbdbf145ec26065
minus31 <donotgetgreed@gmail.com>
2019-06-14 20:18:30 +0900

    Add camera, scaler modules

---
+++ scaler.py
@@ -0,0 +1,20 @@
+# load Dataset
+
+from keras.models import load_model
+import numpy as np 
+import pickle 
+
+# load dataset 
+# with open('../sample_image/sample560_crop_df.bin', 'rb') as f :
+#     crop_img = pickle.load(f)
+    
+model = load_model('../model_hdf5/DenseNet_0720_50Epochs.hdf5')
+
+def scaler(img):
+
+    weigh = model.predict(img)
+    print(weigh)
+    return weigh
+
+if __name__ == "__main__":
+    
