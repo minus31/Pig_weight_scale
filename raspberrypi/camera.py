f8d4e7ab66c2832d4a2d73d7a893a9b80fe3e8ca
minus31 <donotgetgreed@gmail.com>
2019-06-28 20:36:04 +0900

    modify path

--- raspberrypi/camera.py
+++ raspberrypi/camera.py
@@ -104,7 +104,7 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=
         new_frame = cv2.resize(frame_expanded[0], (550, 250))
         weigh = scaler(np.expand_dims(new_frame, axis=0))[0][0]
         now = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
-        camera.capture(RESULT_PATH + "{}.png".format(now + str(weigh)))
+        camera.capture(RESULT_PATH + "-{0:.2f}.png".format(now + str(weigh)))
 
         print(weigh)
 
